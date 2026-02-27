22894dea97a1f57c51eab2d0ab674e62f95faf35
Sneha Kumari <sneha.kumari@testingxperts.com>
2026-02-27 18:34:28 +0530

    dbconnection

--- StudentAPI/Repositry/ClassesRepositry.cs
+++ StudentAPI/Repositry/ClassesRepositry.cs
@@ -49,17 +49,68 @@ namespace StudentAPI.Repositry
 
         public ClassesModel GetById(int id)
         {
+            if(conn.State == System.Data.ConnectionState.Open)
+            {
+                conn.Close();
+            }
+
+            ClassesModel classes = null;
+
+            string query = "SELECT * FROM classes WHERE id = @id";
+
+            using var cmd = new MySqlCommand(query,conn);
+
+            cmd.Parameters.AddWithValue("@id", id);
 
 
+            using var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                classes = new ClassesModel
+                {
+                    classname = (string)reader["id"],
+                    section = (string)reader["section"]
+
+                };
+            }
+            conn.Close();
+            return classes;
 
         }
-        public void Create(ClassesModel class)
+
+
+        public void Create(ClassesModel classes)
         {
+            if(conn.State == System.Data.ConnectionState.Open)
+            {
+                conn.Close();
+            }
 
-         }
+            conn.Open();
 
+            string query = "INSERT INTO classes(classname,section) VALUES(@classname,@section)";
+
+            using var cmd = new MySqlCommand(query, conn);
+
+            cmd.Parameters.AddWithValue("@classname", classes.classname);
+            cmd.Parameters.AddWithValue("@section",classes.section);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
         public void Update(int id)
         {
+            if(conn.State == System.Data.ConnectionState.Open)
+            {
+                conn.Close();
+            }
+
+            conn.Open();
+
+            string query = "UPDATE classes SET classaname = @classname,section=@section WHERE id = @id";
+            using var cmd = new MySqlCommand(query, conn);
+
+            cmd.Parameters.AddWithValue("@classname", classname);
 
         }
 
